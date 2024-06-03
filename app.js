0c6528648032776fc83bb46ec04cc960a58acfd5
Ashwinikhadse31 <ashwinikhadse22@navgurukul.org>
2024-06-03 22:13:23 +0530

    new changes

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
 let input = document.getElementById("moviename");
 let button = document.getElementById("btn");
-let output1 = document.querySelector('.output1');
-let output2 = document.querySelector('.output2');
-let output3 = document.querySelector('.output3');
-let output4 = document.querySelector('.output4');
-let output5 = document.querySelector('.output5');
-let output6 = document.querySelector('.output6');
-let output7 = document.querySelector('.output7');
-let output8 = document.querySelector('.output8');
+let output = document.getElementById("opt");
+
 
 let apiKey = `98add95b`;
 
 button.addEventListener("click", () => {
+    output.innerHTML="";
     fetch(`http://www.omdbapi.com/?t=${input.value}&apikey=${apiKey}`)
-    .then(response => response.json())
+    .then(response => 
+        response.json())
+
     .then(data => {
         console.log(data)
         if (data.Response === "True") {
-            output1.innerHTML = `<img src="${data.Poster}" alt="Movie Poster">`;
-            output2.innerHTML = `<b>Title:</b> ${data.Title}`;
-            output3.innerHTML = `<b>Director:</b> ${data.Director}`;
-            output4.innerHTML = `<b>Year:</b> ${data.Year}`;
-            output5.innerHTML = `<b>Cast:</b> ${data.Actors}`;
-            output6.innerHTML = `<b>BoxOffice:</b> ${data.BoxOffice}`;
-            output7.innerHTML = `<b>Writer:</b> ${data.Writer}`;
-            output8.innerHTML = `<b>Plot:</b> ${data.Plot}`;
+            output.innerHTML = `<img src="${data.Poster}" alt="Movie Poster"><br>
+            <b>Title:</b> ${data.Title}<br>
+            <b>Director:</b> ${data.Director}<br>
+            <b>Year:</b> ${data.Year}<br>
+            <b>Cast:</b> ${data.Actors}<br>
+            <b>BoxOffice:</b> ${data.BoxOffice}<br>
+            <b>Writer:</b> ${data.Writer}<br>
+            <b>Plot:</b> ${data.Plot}`
         } else {
-            output1.innerHTML = `<p>${data.Error}</p>`;
+            output.innerHTML = `<p>${data.Error}</p>`;
         }
     })
     .catch(error => {
         console.log('There has been a problem', error);
-        output1.innerHTML = `<p>Failed to fetch movie data.</p>`;
+        output.innerHTML = `<p>Failed to fetch movie data.</p>`;
     });
 });
